0172b5e5e8edea505154f294c9141aec4a727348
Sergio <sergioscardigno82@gmail.com>
2024-06-13 14:52:31 -0300

    colores e idiomas

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // index.php
 session_start();
-require "lang/main.php";
+require 'lang/main.php';
 
 // Detect language change
 if (isset($_GET['lang'])) {
@@ -19,16 +19,36 @@ $translations = loadLanguage($lang);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Multilanguage Page</title>
+    <link href="js/css/style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body class="bg-gray-100 text-gray-800">
-    <div class="container mx-auto p-4">
-        <div class="flex justify-end">
-            <a href="?lang=en" class="px-4 py-2 <?php echo $lang == 'en' ? 'text-blue-500' : 'text-gray-500'; ?>">English</a>
-            <a href="?lang=es" class="px-4 py-2 <?php echo $lang == 'es' ? 'text-blue-500' : 'text-gray-500'; ?>">Español</a>
+
+
+<body class="bg-custom-gradient text-gray-800">
+    <header id="content" >
+        <div class="container mx-auto flex justify-between items-center">
+        <!-- Logo a la izquierda -->
+            <div>
+                <img id="left-logo" src="#" alt="Left Logo" class="w-60 h-auto">
+            </div>
+
+            <!-- Logo a la derecha -->
+            <div>
+                <!-- <img id="right-logo" src="#" alt="Right Logo" class="w-20 h-auto" style="margin-top:-20px;"> -->
+            </div>
         </div>
-        <h1 class="text-4xl font-bold mb-4"><?php echo $translations['welcome']; ?></h1>
-        <p class="text-lg"><?php echo $translations['description']; ?></p>
+    </header>
+
+
+    <!-- Hero Section con Imagen de Fondo -->
+    <div class="bg-cover bg-center h-screen slider-hero" style="background-image: url('src/img/slider_webp.webp');">
+        <div class="flex items-center justify-center h-full bg-black bg-opacity-20">
+            <div class="text-center text-white px-6 md:px-12">
+                <h2 class="text-xl md:text-3xl font-bold mb-4">Construimos conexiones que te llevan a donde querés estar</h2>
+            </div>
+        </div>
+    </div>
+
     </div>
 </body>
 </html>
